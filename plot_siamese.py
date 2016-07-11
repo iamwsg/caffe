50f9b0b8cd336052b7e804fec6e88e548e9ce6bb
iamwsg <iamwsg@gmail.com>
2016-07-11 11:17:57 -0400

    cpu mode

---
+++ plot_siamese.py
@@ -0,0 +1,15 @@
+#./tools/extra/parse_log.py siamese_train.log .
+import pandas as pd
+import matplotlib.pyplot as plt
+train_log = pd.read_csv("./siamese_train.log.train")
+test_log = pd.read_csv("./siamese_train.log.test")
+_, ax1 = plt.subplots(figsize=(15, 10))
+#ax2 = ax1.twinx()
+ax1.plot(train_log["NumIters"], train_log["loss"], alpha=0.4)
+ax1.plot(test_log["NumIters"], test_log["loss"], 'g')
+#ax2.plot(test_log["NumIters"], test_log["loss"], 'r')
+ax1.set_xlabel('iteration')
+ax1.set_ylabel('train loss')
+#ax2.set_ylabel('test loss')
+plt.grid()
+plt.show()
